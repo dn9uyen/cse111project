7caa5cf31eac1a6e67cf3eff2d4fed20635b2a19
dn9uyen <dannynguyen84268@gmail.com>
2024-12-08 17:59:06 -0800

    In progress

--- backend/backend.py
+++ backend/backend.py
@@ -15,23 +15,23 @@ engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 def createTables():
     with open("createTables.sql", "r") as file:
         script = file.read()
-        statements = script.split(';')
+        statements = script.split(";")
     with engine.connect() as conn:
         for statement in statements:
             # statement = statement.strip()
             conn.execute(text(statement))
         conn.commit()
+
 
 def loadData():
     with open("loadData.sql", "r") as file:
         script = file.read()
-        statements = script.split(';')
+        statements = script.split(";")
     with engine.connect() as conn:
         for statement in statements:
             # statement = statement.strip()
             conn.execute(text(statement))
         conn.commit()
-
 
 
 @app.route("/cpu/info", methods=["GET"])
@@ -43,18 +43,20 @@ def getCpuInfo():
         return response
     with engine.connect() as conn:
         query = """
-            SELECT cpu.*, brand.name AS brand, socket.name AS socket
+            SELECT cpu.*, brand.name AS brand, socket.name AS socket,
+                GROUP_CONCAT(ddrgen.name, ', ') AS ddrgen
             FROM cpu
             JOIN brand ON cpu.brandid = brand.brandid
             JOIN socket on cpu.socketid = socket.socketid
-            WHERE cpuid = ?
+            JOIN cpu_ddrgen ON cpu.cpuid = cpu_ddrgen.cpuid
+            JOIN ddrgen ON cpu_ddrgen.ddrgenid = ddrgen.ddrgenid
+            WHERE cpu.cpuid = ?
         """
         data = conn.exec_driver_sql(query, (cpuid,)).mappings().fetchone()
         if data == None:
             response = flask.jsonify({"error": "Invalid cpuid"})
             response.status_code = 404
             return response
-    print(data)
     jsonData = {
         "cpuid": data["cpuid"],
         "price": data["price"],
@@ -67,12 +69,22 @@ def getCpuInfo():
         "wattage": data["wattage"],
         "hascooler": data["hascooler"],
         "brand": data["brand"],
-        "socket": data["socket"]
+        "socket": data["socket"],
+        "ddrgen": data["ddrgen"].split(",")
     }
     return jsonify(jsonData)
 
 
+@app.route("/cpu/info", methods=["POST"])
+def addCpu():
+    body = flask.request.get_json()
+    with engine.connect() as conn:
+    # for col, val in body.items():
+
+    return "a"
+
+
 if __name__ == "__main__":
-    # createTables()
+    createTables()
     loadData()
     app.run()
--- backend/loadData.sql
+++ backend/loadData.sql
@@ -15,8 +15,14 @@
 -- .import 'data/motherboard_storage_interface.tbl' motherboard_storage_interface
 -- .import 'data/cpu_ddrgen.tbl' cpu_ddrgen
 INSERT OR IGNORE INTO cpu (cpuid, price, model, speed, boostspeed, cores, threads, tdp, wattage, hasgraphics, hascooler, brandid, socketid)
-VALUES (0, 299.99, 'Intel Core i5-10400', 2.9, 4.3, 6, 12, 65, 125, 1, 1, 1, 1);
+VALUES (1, 299.99, 'Intel Core i5-10400', 2.9, 4.3, 6, 12, 65, 125, 1, 1, 1, 1);
 
 INSERT OR IGNORE INTO brand (brandid, name) VALUES (1, 'Intel');
 
 INSERT OR IGNORE INTO socket (socketid, name) VALUES (1, 'LGA1200');
+
+INSERT OR IGNORE INTO ddrgen (ddrgenid, name) VALUES (1, 'DDR4');
+INSERT OR IGNORE INTO ddrgen (ddrgenid, name) VALUES (2, 'DDR5');
+
+INSERT OR IGNORE INTO cpu_ddrgen (cpuid, ddrgenid) VALUES (1, 1);
+INSERT OR IGNORE INTO cpu_ddrgen (cpuid, ddrgenid) VALUES (1, 2);
